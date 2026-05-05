64c1c3c76fea26e7ab91fb430054ca9bc2d96b88
Km Anjli Kanaujiya <kmanjli.kanaujiya@hcltech.com>
2026-05-05 16:58:43 +0530

    code update

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('CI/CD POC Application is running successfully on Feature branch..Done');
+  res.send('CI/CD POC Application is running successfully on Feature branch');
 });
 
 // Start server ONLY when running app directly
